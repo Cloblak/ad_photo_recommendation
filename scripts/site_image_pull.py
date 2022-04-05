eab1f51a8fc9f13e05866ec020f8772062d5c9d3
Cloblak <christopher.oblak@gmail.com>
2022-04-05 14:00:21 -0400

    code and req updated

--- scripts/site_image_pull.py
+++ scripts/site_image_pull.py
@@ -10,10 +10,10 @@ import click
 
 # Pass the headers you want to retrieve from the xml such as ["loc", "lastmod"]
 def parse_sitemap(url, headers):
-    
+
     resp = requests.get(url)
     # we didn't get a valid response, bail
-    if (200 != resp.status_code):
+    if 200 != resp.status_code:
         return False
 
     # BeautifulSoup to parse the document
@@ -24,7 +24,6 @@ def parse_sitemap(url, headers):
     sitemaps = soup.findAll('sitemap')
     new_list = ["Source"] + headers
     panda_out_total = pd.DataFrame([], columns=new_list)
-
 
     if not urls and not sitemaps:
         return False
@@ -54,7 +53,7 @@ def parse_sitemap(url, headers):
                 loc = loc.string
             values.append(loc)
         out.append(values)
-    
+
     # Creates a dataframe
     panda_out = pd.DataFrame(out, columns= new_list)
 
@@ -65,28 +64,29 @@ def parse_sitemap(url, headers):
     #returns the dataframe
     return panda_out
 
-
 if __name__ == "__main__":
-    
+
     print("Preparing for Training Of Model")
-    parent_url = click.prompt("URL of Homepage (www.<sitename>.com): ", type=str, default="www.psychobunny.com")
-    csv_name_path = click.prompt("CSV of Image URL Reference Name:", type=str, default="default_image")
-    
+    parent_url = click.prompt("URL of Homepage (www.<sitename>.com): ", 
+                              type=str, default="www.psychobunny.com")
+    csv_name_path = click.prompt("CSV of Image URL Reference Name:", 
+                                 type=str, default="default_image")
+
     #format url correct
     while parent_url.split(".")[0] != "www":
-        parent_url = click.prompt("Please formate URL correctly -> 'www.<sitename>.com:': ", type=str, default="www.psychobunny.com")
-  
+        parent_url = click.prompt("Please formate URL correctly -> 'www.<sitename>.com:': ", 
+                                  type=str, default="www.psychobunny.com")
+
     complete_url = f'https://{parent_url}/sitemap.xml'
-    
+
     site_links_df = parse_sitemap(complete_url, ["loc"])
-    
+
     column_names = ["Image_Name", "URL"]
     image_url_df = pd.DataFrame()
 
     for url in site_links_df["loc"]:
-
+        
         soup = Soup.get(url)
-
         image_containers = soup.find('img', {'class':'Image--lazyLoad Image--fadeIn'})
 
         try:
@@ -96,7 +96,8 @@ if __name__ == "__main__":
 
                         image_url = "https:" + image.attrs[t]
                         image_name = url.split("/")[-1]
-                        to_concat = pd.DataFrame([[image_name, image_url]], columns = column_names)
+                        to_concat = pd.DataFrame([[image_name, image_url]], 
+                                                 columns = column_names)
 
                         if image_url_df.empty:
                             image_url_df = to_concat                    
@@ -107,7 +108,8 @@ if __name__ == "__main__":
 
                         image_url = "https:" + image.attrs[t]
                         image_name = url.split("/")[-1]
-                        to_concat = pd.DataFrame([[image_name, image_url]], columns = column_names)
+                        to_concat = pd.DataFrame([[image_name, image_url]], 
+                                                 columns = column_names)
 
                         if image_url_df.empty:
                             image_url_df = to_concat                    
